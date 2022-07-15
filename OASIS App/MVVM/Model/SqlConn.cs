84b3df797d0904b98005f587c1352f7d7d92ba6f
Daniel Serga <daniil.serga@gmail.com>
2022-07-15 14:17:07 +0300

    deleted comments from SqlConn class

--- OASIS App/MVVM/Model/SqlConn.cs
+++ OASIS App/MVVM/Model/SqlConn.cs
@@ -6,14 +6,10 @@ namespace OASIS_App.MVVM.Model
 {
     public static class SqlConn
     {
-        //public static SqlConnection Connection => new SqlConnection(ConfigurationManager.ConnectionStrings["Tours"].ConnectionString);
         public static SqlConnection Connection => GetSqlConnection();
 
         public static SqlConnection GetSqlConnection()
         {
-            // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="C:\Users\User\Source\Repos\DaniilSerga\OASIS-App\OASIS App\DataBase\Tours.mdf";Integrated Security=True
-            //                                                     "C:\\Users\\User\\Source\\Repos\\DaniilSerga\\OASIS-App\\OASIS App\\DataBase\\Tours.mdf"
-            // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="C:\Users\Supre\source\repos\OASIS\OASIS App\DataBase\Tours.mdf";Integrated Security=True
             string dbPath = Path.GetFullPath("..\\..\\DataBase\\Tours.mdf");
             
             return new SqlConnection($"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={dbPath};Integrated Security=True");
